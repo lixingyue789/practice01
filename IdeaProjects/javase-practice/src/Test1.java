79e8b940485583201a4b6e14f3d7b3a08e2f0622
lixingyue789 <1852574886@qq.com>
2019-11-05 22:51:21 +0800

    practice

---
+++ IdeaProjects/javase-practice/src/Test1.java
@@ -0,0 +1,63 @@
+/**
+ * Author:lxy1999
+ * Created:2019/3/29
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        //Func1(5);
+        //Func2(5);
+        Func4(5);
+
+    }
+
+    /*public  static void Func1(int N) {//n*n+2n+10
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        for (int k = 0; k < 2 * N; k++) {
+            count++;
+        }
+        System.out.println(count);
+        int M = 10;
+        while ((M--) > 0) {
+            count++;
+        }
+        System.out.println(count);
+    }
+    public static void Func2(int N) {//2n+10
+        int count = 0;
+        for (int k = 0; k < 2 * N ; k++) {
+            count++;
+        }
+        System.out.println(count);
+        int M = 10;
+        while ((M--) > 0) {
+            count++;
+        }
+        System.out.println(count);
+        public static void Func3(int N, int M) {//m+n
+            int count = 0;
+            for (int k = 0; k < M; k++) {
+                count++;
+            }
+            for (int k = 0; k < N ; k++) {
+                count++;
+            }
+            System.out.println(count);*/
+    public static void Func4(int N) {//100
+        int count = 0;
+        for (int k = 0; k < 100; k++) {
+            count++;
+        }
+        System.out.println(count);
+
+    }
+}
+
+
