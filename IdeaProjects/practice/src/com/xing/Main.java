07322b8ecaaaf3a13cf7f031d02a9375d3e52ad8
lixingyue789 <1852574886@qq.com>
2019-11-12 14:10:14 +0800

    practice

---
+++ IdeaProjects/practice/src/com/xing/Main.java
@@ -0,0 +1,22 @@
+package com.xing;
+
+import java.util.Scanner;
+
+/**
+ * Author:lxy1999
+ * Created:2019/11/11
+ */
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            char[] ch = sc.nextLine().toCharArray();
+            String str = sc.nextLine();
+            for(int i=0;i<ch.length;i++){
+                if(!str.contains(String.valueOf(ch[i]))){
+                    System.out.print(ch[i]);
+                }
+            }
+        }
+    }
+}
