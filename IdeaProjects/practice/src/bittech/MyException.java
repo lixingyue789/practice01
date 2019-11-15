621758fd7b43f006a3c503dd448429c729dade3d
lixingyue789 <1852574886@qq.com>
2019-11-15 16:18:24 +0800

    practice

---
+++ IdeaProjects/practice/src/bittech/MyException.java
@@ -0,0 +1,11 @@
+package bittech;
+
+/**
+ * Author:lxy1999
+ * Created:2019/11/7
+ */
+public class MyException extends Exception {
+    public MyException(String smg) {
+        super("捕捉到异常");
+    }
+}
