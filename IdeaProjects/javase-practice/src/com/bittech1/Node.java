074db681af9c8020882b8fdf67b21f52961fa56d
lixingyue789 <1852574886@qq.com>
2019-11-28 16:43:27 +0800

    practice

---
+++ IdeaProjects/javase-practice/src/com/bittech1/Node.java
@@ -0,0 +1,17 @@
+package com.bittech1;
+
+/**
+ * Author:lxy1999
+ * Created:2019/4/20
+ */
+public class Node {
+    public char value;
+    public Node left;
+    public Node right;
+
+    Node(char value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
