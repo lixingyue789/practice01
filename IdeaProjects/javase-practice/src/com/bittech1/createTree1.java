043b3befdefc84afd2c69e7aa5c8b17fa7720cec
lixingyue789 <1852574886@qq.com>
2019-11-29 08:19:41 +0800

    practice

---
+++ IdeaProjects/javase-practice/src/com/bittech1/createTree1.java
@@ -0,0 +1,36 @@
+package com.bittech1;
+
+import java.util.Arrays;
+
+/**
+ * Author:lxy1999
+ * Created:2019/4/18
+ * 未完待续 中序与后序构造二叉树
+ */
+public class createTree1 {
+    private static class Node{
+        char value;
+        Node left;
+        Node right;
+        Node(char v){
+            this.value = v;
+        }
+    }
+
+    private static int find(char[] array,int v){
+        for(int i = 0;i < array.length;i++){
+            if(array[i] == v){
+                return i;
+            }
+        }
+        return -1;
+    }
+    Node buildTree(char[] inOrder,char[] postOrder){
+        char rootValue = postOrder[postOrder.length - 1];
+        Node root = new Node(rootValue);
+        int leftSize = find(inOrder,rootValue);
+        char[] leftInorder = Arrays.copyOfRange(inOrder,0,leftSize);
+        char[] leftPostOrder = Arrays.copyOfRange(postOrder,0,leftSize);
+         return root;
+    }
+}
