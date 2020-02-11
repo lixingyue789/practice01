f9f7de686b729c64cb27ab7925ca553c201d0396
lixingyue789 <1852574886@qq.com>
2020-02-11 19:42:46 +0800

    practice

---
+++ IdeaProjects/day01/src/Test2.java
@@ -0,0 +1,14 @@
+/**
+ * Author:lxy1999
+ * Created:2020/2/6
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        TestRunnable th = new TestRunnable();
+        Thread thread = new Thread(th);
+        thread.start();
+        for(int i = 100;i<110;i++){
+            System.out.println("main方法的任务"+i);
+        }//线程潜在的调度
+    }
+}
