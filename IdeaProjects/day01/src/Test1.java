243c9e767b5e90d754e66894d5f48dedd5d79895
lixingyue789 <1852574886@qq.com>
2020-02-11 19:34:23 +0800

    practice

---
+++ IdeaProjects/day01/src/Test1.java
@@ -0,0 +1,13 @@
+/**
+ * Author:lxy1999
+ * Created:2020/2/6
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        TestThread th = new TestThread();
+        th.start();
+        for(int i = 100;i<110;i++){
+            System.out.println("main方法的任务"+i);
+        }
+    }
+}
