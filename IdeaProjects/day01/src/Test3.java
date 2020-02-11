bbf8ed350c8a44800b59aa1b0fb81e338479aa2f
lixingyue789 <1852574886@qq.com>
2020-02-11 19:43:09 +0800

    practice

---
+++ IdeaProjects/day01/src/Test3.java
@@ -0,0 +1,15 @@
+/**
+ * Author:lxy1999
+ * Created:2020/2/6
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        new Thread(){//Thread是父类，括号内是子类对象，匿名内部类
+            public void run(){
+                for(int i = 0;i<10;i++){
+                    System.out.println("线程+"+getName()+i);
+                }
+            }
+        }.start();
+    }
+}
