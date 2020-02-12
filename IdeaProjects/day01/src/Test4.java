687fd45e5eeb856bf43c4e4e1a52bb9918159e1d
lixingyue789 <1852574886@qq.com>
2020-02-12 19:56:11 +0800

    practice

---
+++ IdeaProjects/day01/src/Test4.java
@@ -0,0 +1,17 @@
+/**
+ * Author:lxy1999
+ * Created:2020/2/6
+ */
+public class Test4 {
+    public static void main(String[] args) {
+        Runnable r = new Runnable(){//Thread是父类，括号内是子类对象，匿名内部类
+            public void run(){
+                for(int i = 0;i<10;i++){
+                    System.out.println("线程+"+Thread.currentThread().getName()+i);
+                }
+            }
+        };
+        Thread t = new Thread(r);
+        t.start();
+    }
+}
