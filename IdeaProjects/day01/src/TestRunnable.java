31b09f699bde2c614dd8ad5a9f78fc8cb19c24ba
lixingyue789 <1852574886@qq.com>
2020-02-12 19:56:30 +0800

    practice

---
+++ IdeaProjects/day01/src/TestRunnable.java
@@ -0,0 +1,12 @@
+/**
+ * Author:lxy1999
+ * Created:2020/2/6
+ */
+public class TestRunnable implements Runnable {
+    @Override
+    public void run() {
+        for(int i = 0;i<10;i++){
+            System.out.println("线程+"+i);
+        }
+    }
+}
