e79f79c17a823439d27f7704bd48047f7d11c5b3
lixingyue789 <1852574886@qq.com>
2020-02-16 11:55:22 +0800

    practice

---
+++ IdeaProjects/day01/src/Test6.java
@@ -0,0 +1,21 @@
+/**
+ * Author:lxy1999
+ * Created:2020/2/14
+ */
+class Person {
+    public void print() {
+        System.out.println("[PRINT]方法：" + this);
+    }
+}
+
+public class Test6 {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        System.out.println("[MAIN]方法：" + p1);
+        p1.print();
+        System.out.println("=================");
+        Person p2 = new Person();
+        System.out.println("[MAIN]方法：" + p2);
+        p2.print();
+    }
+}
