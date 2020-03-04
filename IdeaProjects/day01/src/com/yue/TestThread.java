f0932e780780f4d548dad7fd894fbee90c86c037
lixingyue789 <1852574886@qq.com>
2020-03-04 12:29:28 +0800

    practice

---
+++ IdeaProjects/day01/src/com/yue/TestThread.java
@@ -0,0 +1,13 @@
+package com.yue;
+
+/**
+ * Author:lxy1999
+ * Created:2020/2/5
+ */
+public class TestThread extends Thread{
+    public void run(){
+        for(int i = 0;i<10;i++){
+            System.out.println("线程+"+i);
+        }
+    }
+}
