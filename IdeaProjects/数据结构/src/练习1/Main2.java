e645ee9cf5163295b67bee515c6418434660c1ba
lixingyue789 <1852574886@qq.com>
2020-03-13 07:59:46 +0800

    practice

---
+++ IdeaProjects/数据结构/src/练习1/Main2.java
@@ -0,0 +1,37 @@
+package 练习1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Author:lxy1999
+ * Created:2020/3/4
+ */
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        List<List<String>> list = new ArrayList<>();
+        int r = 0;
+        for(int i = 0;i<=num;i++){
+            String a = String.valueOf(i);
+            List<String> l = new ArrayList<>();
+            l.add(a);
+            list.add(l);
+        }
+        for(int i = 0;i<list.size();i++){
+            List<String> b = list.get(i);
+            for(int j = 0;j<b.size();j++){
+                String c = b.get(j);
+                for(int k = 0;k<c.length();k++){
+                    char ch = c.charAt(k);
+                    if(ch=='2'){
+                        r++;
+                    }
+                }
+            }
+        }
+        System.out.println(r);
+    }
+}
