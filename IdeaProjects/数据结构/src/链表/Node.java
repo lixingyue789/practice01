b6c831cd3209ac325454d0e10980a72a178615e3
lixingyue789 <1852574886@qq.com>
2020-03-26 08:03:20 +0800

    practice

---
+++ IdeaProjects/数据结构/src/链表/Node.java
@@ -0,0 +1,16 @@
+package 链表;
+
+/**
+ * Author:lxy1999
+ * Created:2020/2/27
+ * 带头结点的单链表
+ */
+public class Node{
+    public int value;
+    public Node next;
+    public Node(int value){
+        this.value = value;
+        this.next = null;
+    }
+
+}
