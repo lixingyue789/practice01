b2d6f19f89a2e042d0f8b1cbab392f06c1629015
lixingyue789 <1852574886@qq.com>
2020-04-10 10:23:37 +0800

    practice

---
+++ IdeaProjects/数据结构/src/练习2/Main16.java
@@ -0,0 +1,19 @@
+package 练习2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author:lxy1999
+ * Created:2020/3/27
+ */
+public class Main16 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(4);
+        list.add(3);
+        list.add(2);
+        list.add(1);
+        System.out.println(list);
+    }
+}
