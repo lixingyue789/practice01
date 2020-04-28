28c8a57d8410df63824134cb5455f1d26185c8bd
lixingyue789 <1852574886@qq.com>
2020-04-28 19:47:31 +0800

    practice

---
+++ IdeaProjects/数据结构/src/练习3/Main11.java
@@ -0,0 +1,15 @@
+package 练习3;
+
+import java.util.Scanner;
+
+/**
+ * Author:lxy1999
+ * Created:2020/4/1
+ */
+public class Main11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println("happy");
+    }
+}
