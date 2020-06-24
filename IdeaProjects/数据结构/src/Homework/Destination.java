78e574ce1d7baa6b0297ec51ba7078c782995106
lixingyue789 <1852574886@qq.com>
2020-06-24 14:23:18 +0800

    practice

---
+++ IdeaProjects/数据结构/src/Homework/Destination.java
@@ -0,0 +1,10 @@
+package Homework;
+
+/**
+ * Author:lxy1999
+ * Created:2020/5/17
+ */
+interface Destination {
+    public static final String label="";
+    public abstract String readLabel();
+}
